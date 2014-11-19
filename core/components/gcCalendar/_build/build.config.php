6be01e0fea2725452c85212db5d6047188636096
Mat Dave Jones <mat@matdave.com>
2014-11-19 16:39:21 -0600

    Update build.config.php

--- core/components/gcCalendar/_build/build.config.php
+++ core/components/gcCalendar/_build/build.config.php
@@ -1,6 +1,6 @@
 <?php
 define('MODX_BASE_PATH', 'XXX');
 define('MODX_CORE_PATH', MODX_BASE_PATH . 'core/');
-define('MODX_MANAGER_PATH', MODX_BASE_PATH . 'public_html/g/');
-define('MODX_CONNECTORS_PATH', MODX_BASE_PATH . 'public_html/c/');
+define('MODX_MANAGER_PATH', MODX_BASE_PATH . 'public_html/manager/');
+define('MODX_CONNECTORS_PATH', MODX_BASE_PATH . 'public_html/connectors/');
 define('MODX_ASSETS_PATH', MODX_BASE_PATH . 'public_html/assets/');
