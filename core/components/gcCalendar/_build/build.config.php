44d7f001bc4c039b733b7ab3b5fc2c368a512eb1
Mat Dave Jones <mat@matdave.com>
2014-11-19 16:40:07 -0600

    Delete build.config.php

--- core/components/gcCalendar/_build/build.config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('MODX_BASE_PATH', 'XXX');
-define('MODX_CORE_PATH', MODX_BASE_PATH . 'core/');
-define('MODX_MANAGER_PATH', MODX_BASE_PATH . 'public_html/manager/');
-define('MODX_CONNECTORS_PATH', MODX_BASE_PATH . 'public_html/connectors/');
-define('MODX_ASSETS_PATH', MODX_BASE_PATH . 'public_html/assets/');
